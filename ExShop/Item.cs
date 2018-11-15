32b24716a4da149a326f5b5156d678800ab62b46
Maciej Dobrowolski <mac99dobrowolski@gmail.com>
2018-11-15 19:06:45 +0100

    Zatweirdzam zmiany

--- ExShop/Item.cs
+++ ExShop/Item.cs
@@ -10,7 +10,7 @@ namespace ExShop
     {
     
         public int Id { get; set; }
-        public string Name { get; set }
+        public string Name { get; set; }
         public int Cetegory { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
